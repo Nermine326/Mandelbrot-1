50119ab2877e580e59ce586c27413a199e577235
arkano29 <40850258+arkano29@users.noreply.github.com>
2019-08-21 17:40:30 +0200

    3D plot
    Now another function is available, plot_3d(), which plots the mandelbrot set in 3D.

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,17 +1,9 @@
-import os
-import matplotlib.pyplot as plt
-import random
-import numpy as np
-import time
-
-
 #Object to obtain and plot Mandelbrot set by : Arkaitz Bidaurrazaga Barrueta
 #z0 will be the initial value and a will be the varying complex number that will be checked : z_i+1=z_i^2+a
 #The cutoff criteria may depend on the modulus and interation number
 class Mandelbrot():
     
-    def __init__(self,start = complex(0,0), max_iter = 500, max_modul = 2, N_square = 301, 
-                 N_x = 301, N_y = 301, l_vertex = (-1.5,-1), r_vertex = (0.5,1), cm = plt.cm.RdBu):
+    def __init__(self,start = complex(0,0), max_iter = 500, max_modul = 2, N_square = 301, N_x = 301, N_y = 301, l_vertex = (-1.5,-1), r_vertex = (0.5,1), cm = plt.cm.RdBu):
         self.start = start #Where to start the iteration : z0
         self.set = list() #Here the set will be saved in order to later plot
         self.max_iter = max_iter #Iteration cutoff
@@ -27,8 +19,7 @@ class Mandelbrot():
         self.right_vertex = r_vertex #The vertex of the rectangle in the right-upside
         self.cm = cm #Defines the colormap used to plot the set
         
-    def reset(self,start = complex(0,0), max_iter = 500, max_modul = 2, N_square = 301, 
-              N_x = 301, N_y = 301, l_vertex = (-1.5,-1), r_vertex = (0.5,1), cm = plt.cm.RdBu):
+    def reset(self,start = complex(0,0), max_iter = 500, max_modul = 2, N_square = 301, N_x = 301, N_y = 301, l_vertex = (-1.5,-1), r_vertex = (0.5,1), cm = plt.cm.RdBu):
         self.start = start #Where to start the iteration : z0
         self.set = list() #Here the set will be saved in order to later plot
         self.max_iter = max_iter #Iteration cutoff
@@ -50,8 +41,7 @@ class Mandelbrot():
     def make_set(self): #Generates the set of complex points, the value in the dictionary will be the iteration number obtained from cutoff criteria
         diff_x = (self.right_vertex[0]-self.left_vertex[0])/self.N_x
         diff_y = (self.right_vertex[1]-self.left_vertex[1])/self.N_y
-        self.set = [[self.left_vertex[0]+i*diff_x,self.left_vertex[1]+j*diff_y,0] 
-                    for j in range(self.N_y) for i in range(self.N_x)]
+        self.set = [[self.left_vertex[0]+i*diff_x,self.left_vertex[1]+j*diff_y,0] for j in range(self.N_y) for i in range(self.N_x)]
         
     def plot(self,size = (12,12)): #Plots the set
         fig = plt.figure(figsize=size)
@@ -62,8 +52,16 @@ class Mandelbrot():
         plt.title('Mandelbrot')
         plt.show()
         
-    def save_fig(self,directory = os.path.join(os.getenv("HOME"),"Mandelbrot"),
-                 name = "Mandelbrot", title = "Mandelbrot", file_format = "png"):
+    def plot_3d(self):
+        X = np.array(self.set)[:,0].reshape((self.N_x,self.N_y))
+        Y = np.array(self.set)[:,1].reshape((self.N_x,self.N_y))
+        Z = np.array(self.set)[:,-1].reshape((self.N_x,self.N_y))
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot_surface(X, Y, Z, rstride=1, cstride=1,cmap='viridis',edgecolor='none')
+        plt.show()
+        
+    def save_fig(self,directory = os.path.join(os.getenv("HOME"),"Mandelbrot"),name = "Mandelbrot", title = "Mandelbrot", file_format = "png"):
         os.makedirs(directory, exist_ok=True)
         fig = plt.figure(figsize=(12,12))
         im = plt.imshow(np.array(self.set)[:,-1].reshape((self.N_x,self.N_y)),cmap=self.cm, 
@@ -93,8 +91,7 @@ class Mandelbrot():
         if verbose:
             print("Time: %s s ; %s min " %((end-start),(end-start)/60))
             
-    def compute_save(self,directory = os.path.join(os.getenv("HOME"),"Mandelbrot"),
-                     name = "Mandelbrot", title = "Mandelbrot", file_format = "png",verbose = 0): #Compute and save at once
+    def compute_save(self,directory = os.path.join(os.getenv("HOME"),"Mandelbrot"),name = "Mandelbrot", title = "Mandelbrot", file_format = "png",verbose = 0): #Compute and save at once
         self.make_set()
         self.compute_set(verbose)
         self.save_fig(directory,name,title,file_format)
